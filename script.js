0ac253aef86e6ef57ab3efa966fa3b10199fc173
allanlechineskisilva-rgb <allan.lechineski.silva@escola.pr.gov.br>
2026-08-27 11:41:56 -0300

    ok

---
+++ script.js
@@ -0,0 +1,7 @@
+let botao = document.getElementById("botao");
+let mensagem = document.getElementById("mensagem");
+
+botao.addEventListener("click", function() {
+    mensagem.textContent = "BOOYAH! 🔥 Você clicou no botão!";
+});
+
